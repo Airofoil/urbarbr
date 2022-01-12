618caadabb3a016328688aae58b7133832bbadba
Junjie Guo <junjieguo@JunjiedeMBP-2.modem>
2022-01-12 16:46:06 +1030

    Jesse - Only allow one product in cart, homepage testiminials style fixed

--- woodmart-child/functions.php
+++ woodmart-child/functions.php
@@ -178,3 +178,8 @@ function filter_gettext( $translated, $text, $domain  ) {
 }
 add_filter( 'gettext',  'filter_gettext', 10, 3 );
 
+add_filter( 'woocommerce_add_to_cart_validation', 'bbloomer_only_one_in_cart', 9999, 2 );
+function bbloomer_only_one_in_cart( $passed, $added_product_id ) {
+   wc_empty_cart();
+   return $passed;
+}
