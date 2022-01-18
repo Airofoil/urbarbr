cd7a997b7dbd63622e15b806d446990e573fffbf
JosieHan <61893633+JosieHan@users.noreply.github.com>
2022-01-18 16:23:28 +1030

    home page product list & service page style updates

--- woodmart-child/woocommerce/content-product-tiled.php
+++ woodmart-child/woocommerce/content-product-tiled.php
@@ -33,8 +33,22 @@
 			<div class="jac-products-header-top">
 				<div class="jac-products-header-top-left">
 					<h5 class="jac-barber-name"><?php echo $product_title ?></h5>
-					<?php echo '<div class="star-rating"><span style="width:'.( ( $average / 5 ) * 100 ) . '%"><strong itemprop="ratingValue" class="rating">'.$average.'</strong> '.__( 'out of 5', 'woocommerce' ).'</span></div>'; ?>
-					<?php if ( comments_open() ): ?><a href="<?php echo get_permalink() ?>#reviews" class="woocommerce-review-link" rel="nofollow"><?php printf( _n( '%s',$review_count,'woocommerce' ), '<span class="count">' . esc_html( $review_count ) . '</span>' ); ?> Reviews</a><?php endif ?>
+					<?php if($show_brife_product_tile && get_field( "short_description",$product_id) && get_field( "short_description",$product_id)!=""){
+						echo '<p class="short_description">' . get_field( "short_description",$product_id) . '</p>';
+					} ?>
+					<?php if(!$show_brife_product_tile){
+						echo '<div class="star-rating"><span style="width:'.( ( $average / 5 ) * 100 ) . '%"><strong itemprop="ratingValue" class="rating">'.$average.'</strong> '.__( 'out of 5', 'woocommerce' ).'</span></div>'; 
+					}?>
+					<div><span> <?php printf( _n( '%s',$review_count,'woocommerce' ), '<span class="count">' . esc_html( $review_count ) . '</span>' ); ?>
+					<?php if ( comments_open() && !$show_brife_product_tile ){ echo "Reviews"; } ?> </span>
+					<?php if($show_brife_product_tile){
+						echo '<div class="star-rating"><span style="width:'.( ( $average / 5 ) * 100 ) . '%"><strong itemprop="ratingValue" class="rating">'.$average.'</strong> '.__( 'out of 5', 'woocommerce' ).'</span></div>'; 
+					}?>
+					
+					<?php if($show_brife_product_tile){
+						echo '<span class="barber_location">' . get_field( "barber_location",$product_id) . '</span>';
+					} ?>
+					</div>
 				</div>
 				<?php if(!$show_brife_product_tile){ ?>
 					<div class="jac-products-header-top-right">
@@ -66,7 +80,7 @@
 					echo '<div class="product-element-bottom">';
 					// var_dump($value);
 					echo '<div>' . $value['label'] . '</div>';
-					echo '<div>' . $value['price'] . '</div>';
+					echo '<div>$' . number_format((float)$value['price'], 2, '.', '') . '</div>';
 					
 					echo '</div>';
 				}
@@ -78,7 +92,8 @@
 			display: flex;
     		justify-content: space-between;
 			margin-bottom: var(--wd-tags-mb);
-			color: var(--wd-title-color);
+			color: #284158;
+			font-size:16px;
 			text-transform: var(--wd-title-transform);
 			font-weight: var(--wd-title-font-weight);
 			font-style: var(--wd-title-font-style);
@@ -90,15 +105,80 @@
 			margin-top: 25px;
 		}
 		.jac-barber-name{
-			margin-bottom: 5px;
+			margin-bottom: 0px;
+			color:#292727;
+			font-size:20px;
 		}
 		.jac-products-header-top-right a{
 			color: #284158;
 			font-size: 16px;
 			font-weight: bold;
-			border-bottom: 2px solid #8896A2;
+			/* border-bottom: 2px solid #8896A2; */
     		padding: 0 0 3px;
+			position: absolute;
+			top: 0;
+			left: -5px;
+			width: 110px;
 		}
+		.jac-products-header-top-left{
+			color: #102C45;
+			font-size: 14px;
+			font-weight: 400;
+		}
+		.jac-products-header-top-right {
+			height: 30px;
+			position: relative;
+			border-bottom: 2px solid #8896A2;
+			width: 95px;
+		}
+		.jac-barber-details .woocommerce-review-link{
+			color: #284158;
+			font-weight: bold;
+			font-size: 90%;
+		}
+		.jac-products-header-top-left .star-rating:before{
+			content: "\f149" !important;
+			color:#FFC702 !important;
+		}
+		.jac-products-header-top-left .product-grid-item .star-rating{
+			margin-bottom: 2px;
+		}
+		.archive .product-grid-item.product-type-booking .product-wrapper{
+			padding: 30px;
+    		background-color: #F8F9F9;
+		}
+		.wd-hover-tiled .product-element-bottom{
+			padding: 20px;
+		}
+		.product-element-bottom:last-of-type{
+			margin-bottom:0px;
+		}
+		.brief-product-tile{
+			margin-left:0px;
+			margin-top:10px;
+		}
+		.brief-product-tile .jac-barber-name{
+			font-size: 16px;
+		}
+		.product-grid-item .brief-product-tile .star-rating{
+			margin-bottom: 2px;
+		}
+		.brief-product-tile .jac-products-header-top-left{
+			color: #70808F;
+    		font-size: 14px;
+		}
+		.jac-products-header-top-left > div > span.barber_location{
+			float: right;
+			color:#000000;
+		}
+		.short_description{
+			margin-bottom: 6px;
+		}
+		<?php if(!$show_brife_product_tile): ?>
+			.jac-products-header-top-left > div{
+				display:inline-block;
+			}
+		<?php endif ?>
 	</style>
 	<!-- NOT SURE IF BELOW CODE IS REQUIRED -->
 	<?php
