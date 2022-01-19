f46d72ebda713f025e728541e08dbf25aeb7c49d
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2022-01-19 14:36:18 +1030

    separate login and signup, some stlying wip

--- woodmart-child/functions.php
+++ woodmart-child/functions.php
@@ -167,12 +167,7 @@ add_action( 'template_redirect', 'select_services' );
 function select_services() {
   // Make sure the request is for a user-facing page
   if ( 
-    ! is_singular() && 
-    ! is_page() && 
-    ! is_single() && 
-    ! is_archive() && 
-    ! is_home() &&
-    ! is_front_page() 
+ 	! is_product()
   ) {
     return false;
   }
@@ -201,3 +196,70 @@ function cng_author_base() {
     $author_slug = 'profile';
     $wp_rewrite->author_base = $author_slug;
 }*/
+
+
+// REGISTRATION SHORTCODE
+function wc_registration_form_function() {
+	if ( is_admin() ) return;
+	if ( is_user_logged_in() ) return;
+  
+	ob_start();
+  
+	do_action( 'woocommerce_before_customer_login_form' );
+  
+	?>
+	<div style="width: 50%; margin: 0 auto;">
+	 <form method="post" class="woocommerce-form woocommerce-form-register register" <?php do_action( 'woocommerce_register_form_tag' ); ?> >
+ 
+		 <?php do_action( 'woocommerce_register_form_start' ); ?>
+ 
+		 <?php if ( 'no' === get_option( 'woocommerce_registration_generate_username' ) ) : ?>
+ 
+			 <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+				 <label for="reg_username"><?php esc_html_e( 'Username', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+				 <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="reg_username" autocomplete="username" value="<?php echo ( ! empty( $_POST['username'] ) ) ? esc_attr( wp_unslash( $_POST['username'] ) ) : ''; ?>" /><?php // @codingStandardsIgnoreLine ?>
+			 </p>
+ 
+		 <?php endif; ?>
+ 
+		 <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+			 <label for="reg_email"><?php esc_html_e( 'Email address', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+			 <input type="email" class="woocommerce-Input woocommerce-Input--text input-text" name="email" id="reg_email" autocomplete="email" value="<?php echo ( ! empty( $_POST['email'] ) ) ? esc_attr( wp_unslash( $_POST['email'] ) ) : ''; ?>" /><?php // @codingStandardsIgnoreLine ?>
+		 </p>
+ 
+		 <?php if ( 'no' === get_option( 'woocommerce_registration_generate_password' ) ) : ?>
+ 
+			 <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+				 <label for="reg_password"><?php esc_html_e( 'Password', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+				 <input type="password" class="woocommerce-Input woocommerce-Input--text input-text" name="password" id="reg_password" autocomplete="new-password" />
+			 </p>
+ 
+		 <?php else : ?>
+ 
+			 <p><?php esc_html_e( 'A password will be sent to your email address.', 'woocommerce' ); ?></p>
+ 
+		 <?php endif; ?>
+ 
+		 <?php do_action( 'woocommerce_register_form' ); ?>
+ 
+		 <p class="woocommerce-form-row form-row">
+			 <?php wp_nonce_field( 'woocommerce-register', 'woocommerce-register-nonce' ); ?>
+			 <button type="submit" class="woocommerce-Button woocommerce-button button woocommerce-form-register__submit" name="register" value="<?php esc_attr_e( 'Register', 'woocommerce' ); ?>"><?php esc_html_e( 'Sign up', 'woocommerce' ); ?></button>
+		 </p>
+ 
+		 <?php do_action( 'woocommerce_register_form_end' ); ?>
+ 
+	 </form>
+	 <a href="/my_account">Login here</a>
+	
+	
+	</div>
+	<?php
+	  
+	return ob_get_clean();
+	 
+ }
+ add_shortcode( 'wc_registration_form', 'wc_registration_form_function' );
+
+
+ 
