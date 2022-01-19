f053775c23db1ba1a4485608b240475c6e287e54
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2022-01-19 14:42:45 +1030

    keep ommitted code

--- woodmart-child/functions.php
+++ woodmart-child/functions.php
@@ -112,6 +112,62 @@ function wpse27856_set_content_type(){
 }
 add_filter( 'wp_mail_content_type','wpse27856_set_content_type' );
 
+/**
+
+ * Moving the payments
+
+ */
+
+add_action( 'woocommerce_checkout_shipping', 'my_custom_display_payments', 20 );
+/**
+ * Displaying the Payment Gateways 
+ */
+function my_custom_display_payments() {
+  if ( WC()->cart->needs_payment() ) {
+    $available_gateways = WC()->payment_gateways()->get_available_payment_gateways();
+    WC()->payment_gateways()->set_current_gateway( $available_gateways );
+  } else {
+    $available_gateways = array();
+  }
+  ?>
+  <div id="checkout_payments">
+    <h3><?php esc_html_e( 'Payment Method', 'woocommerce' ); ?></h3>
+    <?php if ( WC()->cart->needs_payment() ) : ?>
+    <ul class="wc_payment_methods payment_methods methods">
+    <?php
+    if ( ! empty( $available_gateways ) ) {
+      foreach ( $available_gateways as $gateway ) {
+        wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $gateway ) );
+      }
+    } else {
+      echo '<li class="woocommerce-notice woocommerce-notice--info woocommerce-info">' . apply_filters( 'woocommerce_no_available_payment_methods_message', WC()->customer->get_billing_country() ? esc_html__( 'Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) : esc_html__( 'Please fill in your details above to see available payment methods.', 'woocommerce' ) ) . '</li>'; // @codingStandardsIgnoreLine
+    }
+    ?>
+    </ul>
+  <?php endif; ?>
+  </div>
+<?php
+}
+/**
+ * Adding the payment fragment to the WC order review AJAX response
+ */
+add_filter( 'woocommerce_update_order_review_fragments', 'my_custom_payment_fragment' );
+
+/**
+ * Adding our payment gateways to the fragment #checkout_payments so that this HTML is replaced with the updated one.
+ */
+function my_custom_payment_fragment( $fragments ) {
+	ob_start();
+
+	my_custom_display_payments();
+
+	$html = ob_get_clean();
+
+	$fragments['#checkout_payments'] = $html;
+
+	return $fragments;
+}
+
 function filter_gettext( $translated, $text, $domain  ) {
   if( $text == 'Your order' && is_checkout() && ! is_wc_endpoint_url() ) {
       // Loop through cart items
