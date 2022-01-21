b554102b7cef95e0f5a10e88e6a186b4fca9e26f
Junjie Guo <junjieguo@JunjiedeMBP-2.modem>
2022-01-21 14:49:49 +1030

    combine select at least one service & check if cart empty or not

--- woodmart-child/js/child-theme.js
+++ woodmart-child/js/child-theme.js
@@ -1,16 +1,22 @@
 jQuery(document).ready(function ($) {
 
-    $('.single_add_to_cart_button').click(function (event) {
-        if ($('input.wc-pao-addon-checkbox:checked').length > 0) {
-            if (!confirm('Are you sure you want to add the product?')) {
-                event.preventDefault();
-                event.stopPropagation();
+    $('.single_add_to_cart_button').click( function( event ) {
+		if($('input.wc-pao-addon-checkbox:checked').length > 0){
+
+            if($('div.wd-cart-empty').length) {
+
+            } else {
+                if( ! confirm( 'Do you want to remove the existing booking and add this to cart?' ) ) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
             }
-        } else {
-            event.preventDefault();
-            event.stopPropagation();
-            confirm("Please select service.");
-        }
+
+		}else{
+			event.preventDefault();
+			event.stopPropagation();
+			confirm("Please select service.");
+		}
     });
 
 });
