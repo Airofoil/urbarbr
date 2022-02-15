0b9633873835a33432e799e6b9412d27ac2640d8
James Douglas-Hill <jdouglas-hill@jamesanthonyconsulting.com.au>
2022-02-15 16:28:11 +1030

    Added missing unpushed code(?) and changed config options of datetimepicker, but still WIP

--- woodmart-child/js/child-theme.js
+++ woodmart-child/js/child-theme.js
@@ -1,4 +1,19 @@
 jQuery(document).ready(function ($){
+	$('input.wc-pao-addon-checkbox').on('click',function(){
+        if ($('input.wc-pao-addon-checkbox:checked').length > 0) {
+            $('.single_add_to_cart_button').removeClass('no-service-disable');
+        }else{
+            $('.single_add_to_cart_button').addClass('no-service-disable');
+        }
+    })
+	  $(".single_add_to_cart_button").ready(function () {
+		  console.log($(".single_add_to_cart_button"));
+        if ($('input.wc-pao-addon-checkbox:checked').length > 0) {
+            $('.single_add_to_cart_button').removeClass('no-service-disable');
+        } else {
+            $('.single_add_to_cart_button').addClass('no-service-disable');
+        }
+    });
 
     $('.single_add_to_cart_button').click( function( event ) {
         if($('input.wc-pao-addon-checkbox:checked').length > 0){
@@ -21,9 +36,10 @@ jQuery(document).ready(function ($){
     
 
     $('#booking-date-search').datetimepicker({
-        sideBySide: true,
-        format: 'YYYY-MM-DD LT',         
-        minDate:new Date(),
+        //sideBySide: true,
+        //format: 'YYYY-MM-DD H:i',         
+        minDate: 0,
+        //maxDate: 60
 //      minDate: moment({h:9}),
 //      maxDate: moment({h:16})
     });
@@ -31,6 +47,7 @@ jQuery(document).ready(function ($){
     setTimeout(function() {
         $("#billing_address_1").attr("placeholder", "Enter address and select from the dropdown")
     }, 100);
+
 //  $.datetimepicker.setDateFormatter('moment');
-    
+
 });
