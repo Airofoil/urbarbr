03dfb2f9ba4d781676da3626c39bb29ef5c4bd84
jae <ghjgjh0107@gamil.com>
2022-03-01 15:46:43 +1030

    added custom plugins

---
+++ plugins/sendex/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'jae-hyuck/sendex',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jae-hyuck/sendex' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '336cc54d70a6e05f397ee25777d79482a4574efd',
+            'dev_requirement' => false,
+        ),
+    ),
+);
