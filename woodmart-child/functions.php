ebb1d014c07daabf7021182a01012da1c17ce421
James Douglas-Hill <jdouglas-hill@jamesanthonyconsulting.com.au>
2022-03-04 18:57:06 +1030

    Added changes and merged from jae-dev

--- woodmart-child/functions.php
+++ woodmart-child/functions.php
@@ -209,7 +209,7 @@ function wc_registration_form_function() {
 	?>
 	<div class="registration-wrapper">
 		<h2 style="text-align: center;">Become a Member</h2>
-		<p style="text-align: center;">Become a member - don't miss out on deals, offers, discounts and bonus vouchers</p>
+		<!-- ----<p style="text-align: center;">Become a member - don't miss out on deals, offers, discounts and bonus vouchers</p> -->
 		<form method="post" class="woocommerce-form woocommerce-form-register register" <?php do_action( 'woocommerce_register_form_tag' ); ?> >
 	
 			<?php do_action( 'woocommerce_register_form_start' ); ?>
@@ -217,7 +217,7 @@ function wc_registration_form_function() {
 			<?php if ( 'no' === get_option( 'woocommerce_registration_generate_username' ) ) : ?>
 	
 				<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-					<label for="reg_username"><?php esc_html_e( 'Username', 'woocommerce' ); ?>&nbsp;<span class="required"></span></label>
+					<label for="reg_username">Name&nbsp;<span class="required"></span></label>
 					<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="reg_username" autocomplete="username" value="<?php echo ( ! empty( $_POST['username'] ) ) ? esc_attr( wp_unslash( $_POST['username'] ) ) : ''; ?>" /><?php // @codingStandardsIgnoreLine ?>
 				</p>
 	
@@ -241,10 +241,9 @@ function wc_registration_form_function() {
 	
 			<?php endif; ?>
 	
-	
-			<p class="woocommerce-form-row form-row">
+			<p class="woocommerce-form-row form-row last">
 				<?php wp_nonce_field( 'woocommerce-register', 'woocommerce-register-nonce' ); ?>
-				<button type="submit" style="width: 30%; border-radius: 3px" class="form-item-right woocommerce-Button woocommerce-button button woocommerce-form-register__submit" name="register" value="<?php esc_attr_e( 'Register', 'woocommerce' ); ?>"><?php esc_html_e( 'Sign up', 'woocommerce' ); ?></button>
+				<button type="submit" style="max-width: 110px;" class="form-item-right woocommerce-Button woocommerce-button button woocommerce-form-register__submit" name="register" value="<?php esc_attr_e( 'Register', 'woocommerce' ); ?>"><?php esc_html_e( 'Sign up', 'woocommerce' ); ?></button>
 				<a class="form-item-left" style="text-decoration: underline;" href="/my-account">Login here</a>
 			</p>
 	
@@ -559,6 +558,117 @@ function bbloomer_checkout_step3( $cart ) {
 	} 
 }
 
+add_action( 'rest_api_init', 'json_data_route');
+function json_data_route() {
+	register_rest_route( 'json_data/v1', '/customer_info', array(
+		'methods' => WP_REST_SERVER::READABLE,
+		'callback' => 'customerData',
+	) );
+}
+
+function customerData() {
+	$chBooking = curl_init();
+    $headers = array(
+		'Accept: application/json',
+		'Content-Type: application/json',
+    );
+	
+	$urlBooking= 'https://staging-urbarbr.kinsta.cloud/wp-json/wc-bookings/v1/bookings?per_page=100&consumer_key=ck_5a1cb710eb2853f8f109830d2d3346b4fef4fd78&consumer_secret=cs_ec2aa8ae576eec5362416a93c3d57e504baca46d';
+	curl_setopt($chBooking, CURLOPT_URL, $urlBooking);
+	curl_setopt($chBooking, CURLOPT_RETURNTRANSFER, 1);
+	$outputBooking = curl_exec($chBooking);
+	try {
+		$jsonBooking = json_decode($outputBooking, true, JSON_THROW_ON_ERROR);
+	} catch (JsonException $e) {
+		throw new EncryptException('Could not encrypt the data.', 0, $e);
+	}
+	curl_close($chBooking);
+	
+	$chOrder = curl_init();
+	$urlOrder= 'https://staging-urbarbr.kinsta.cloud/wp-json/wc/v3/orders?per_page=100&consumer_key=ck_5a1cb710eb2853f8f109830d2d3346b4fef4fd78&consumer_secret=cs_ec2aa8ae576eec5362416a93c3d57e504baca46d';
+	curl_setopt($chOrder, CURLOPT_URL, $urlOrder);
+	curl_setopt($chOrder, CURLOPT_RETURNTRANSFER, 1);
+	$outputOrder = curl_exec($chOrder);
+	try {
+		$jsonOrder = json_decode($outputOrder, true, JSON_THROW_ON_ERROR);
+	} catch (JsonException $e) {
+		throw new EncryptException('Could not encrypt the data.', 0, $e);
+	}
+	curl_close($chOrder);
+
+	$chProduct = curl_init();
+	$urlProduct= 'https://staging-urbarbr.kinsta.cloud/wp-json/wc/v3/products/?per_page=100&consumer_key=ck_5a1cb710eb2853f8f109830d2d3346b4fef4fd78&consumer_secret=cs_ec2aa8ae576eec5362416a93c3d57e504baca46d';
+	curl_setopt($chProduct, CURLOPT_URL, $urlProduct);
+	curl_setopt($chProduct, CURLOPT_RETURNTRANSFER, 1);
+	$outputProduct = curl_exec($chProduct);
+	try {
+		$jsonProduct = json_decode($outputProduct, true, JSON_THROW_ON_ERROR);
+	} catch (JsonException $e) {
+		throw new EncryptException('Could not encrypt the data.', 0, $e);
+	}
+	curl_close($chProduct);
+
+	date_default_timezone_set('Australia/Adelaide');
+	$date = date('Y-m-d H:i:s');
+	$long = strtotime($date);
+
+	$start = $jsonBooking[0]['start'];
+	if (is_countable($jsonBooking)) {
+		for ($i=0; $i < count($jsonBooking); $i++) { //1hr = 3600
+			if (($jsonBooking[$i]['start'] < $long && $jsonBooking[$i]['end'] < $long) || $jsonBooking[$i]['status'] === 'cancelled' || $jsonBooking[$i]['status'] === 'unpaid') {
+				array_splice($jsonBooking, $i, 1);
+				$i = 0;
+			}
+		}
+	}
+
+	$customers = array();
+	if (is_countable($jsonBooking) && is_countable($jsonOrder)) {
+		for ($i=0; $i < count($jsonBooking); $i++) {
+			$jsonBooking[$i]['start'] = $jsonBooking[$i]['start'] - 37800;
+			$jsonBooking[$i]['end'] = $jsonBooking[$i]['end'] - 37800;
+			for ($j=0; $j < count($jsonOrder); $j++) { 
+				if ($jsonBooking[$i]['order_id'] === $jsonOrder[$j]['id']) {
+					array_push($customers, $jsonOrder[$j]);
+				}
+			}
+		}
+	}
+
+	if (is_countable($customers)) {
+		for ($i=0; $i < count($customers); $i++) { 
+			$customers[$i]['billing']['phone'] = str_replace(' ', '', $customers[$i]['billing']['phone']);
+			if ($customers[$i]['billing']['phone'][0] === '0') {
+				$customers[$i]['billing']['phone'] = '+61' . substr($customers[$i]['billing']['phone'], 1);
+			}
+			if ($customers[$i]['billing']['phone'][0] !== '+') {
+				$customers[$i]['billing']['phone'] = '+' . strval($customers[$i]['billing']['phone']);
+			}
+		}
+	}
+
+	$customerData = array();
+	if (is_countable($jsonBooking) && is_countable($jsonProduct)) {
+		for ($i=0; $i < count($jsonBooking); $i++) { 
+			for ($j=0; $j < count($jsonProduct); $j++) { 
+				if ($jsonBooking[$i]['product_id'] === $jsonProduct[$j]['id']) {
+					array_push($customerData, array(
+						'first_name' => $customers[$i]['billing']['first_name'],
+						'last_name' => $customers[$i]['billing']['last_name'],
+						'phone' => $customers[$i]['billing']['phone'],
+						'booking_id' => $jsonBooking[$i]['id'],
+						'order_id' => $jsonBooking[$i]['order_id'],
+						'product_id' => $jsonBooking[$i]['product_id'],
+						'product_name' => $jsonProduct[$j]['name']
+					));
+				}
+			}
+		}
+	}
+
+	return $customerData;
+}
+
 add_action( 'check_booking_time', 'cw_function' );
 function cw_function() {
 	$chBooking = curl_init();
@@ -570,7 +680,11 @@ function cw_function() {
 	curl_setopt($chBooking, CURLOPT_URL, $urlBooking);
 	curl_setopt($chBooking, CURLOPT_RETURNTRANSFER, 1);
 	$outputBooking = curl_exec($chBooking);
-	$jsonBooking = json_decode($outputBooking, true);
+	try {
+		$jsonBooking = json_decode($outputBooking, true, JSON_THROW_ON_ERROR);
+	} catch (JsonException $e) {
+		throw new EncryptException('Could not encrypt the data.', 0, $e);
+	}
 	curl_close($chBooking);
 	
 	$chOrder = curl_init();
@@ -578,53 +692,77 @@ function cw_function() {
 	curl_setopt($chOrder, CURLOPT_URL, $urlOrder);
 	curl_setopt($chOrder, CURLOPT_RETURNTRANSFER, 1);
 	$outputOrder = curl_exec($chOrder);
-	$jsonOrder = json_decode($outputOrder, true);
+	try {
+		$jsonOrder = json_decode($outputOrder, true, JSON_THROW_ON_ERROR);
+	} catch (JsonException $e) {
+		throw new EncryptException('Could not encrypt the data.', 0, $e);
+	}
 	curl_close($chOrder);
+
+	$chProduct = curl_init();
+	$urlProduct= 'https://staging-urbarbr.kinsta.cloud/wp-json/wc/v3/products/?per_page=100&consumer_key=ck_5a1cb710eb2853f8f109830d2d3346b4fef4fd78&consumer_secret=cs_ec2aa8ae576eec5362416a93c3d57e504baca46d';
+	curl_setopt($chProduct, CURLOPT_URL, $urlProduct);
+	curl_setopt($chProduct, CURLOPT_RETURNTRANSFER, 1);
+	$outputProduct = curl_exec($chProduct);
+	try {
+		$jsonProduct = json_decode($outputProduct, true, JSON_THROW_ON_ERROR);
+	} catch (JsonException $e) {
+		throw new EncryptException('Could not encrypt the data.', 0, $e);
+	}
+	curl_close($chProduct);
 
 	date_default_timezone_set('Australia/Adelaide');
 	$date = date('Y-m-d H:i:s');
 	$long = strtotime($date);
 
 	$start = $jsonBooking[0]['start'];
-	for ($i=0; $i < count($jsonBooking); $i++) { //1hr = 3600
-		if (($jsonBooking[$i]['start'] < $long && $jsonBooking[$i]['end'] > $long) || $jsonBooking[$i]['status'] === 'cancelled' || $jsonBooking[$i]['status'] === 'unpaid') {
-			array_splice($jsonBooking, $i, 1);
-			$i = 0;
-		}
-	}
-
-	$customers = array();
-	for ($i=0; $i < count($jsonBooking); $i++) { 
-		$jsonBooking[$i]['start'] = $jsonBooking[$i]['start'] - 37800;
-		$jsonBooking[$i]['end'] = $jsonBooking[$i]['end'] - 37800;
-		for ($j=0; $j < count($jsonOrder); $j++) { 
-			if ($jsonBooking[$i]['order_id'] === $jsonOrder[$j]['id']) {
-				array_push($customers, $jsonOrder[$j]);
+	if (is_countable($jsonBooking)) {
+		for ($i=0; $i < count($jsonBooking); $i++) { //1hr = 3600
+			if (($jsonBooking[$i]['start'] < $long && $jsonBooking[$i]['end'] < $long) || $jsonBooking[$i]['status'] === 'cancelled' || $jsonBooking[$i]['status'] === 'unpaid') {
+				array_splice($jsonBooking, $i, 1);
+				$i = 0;
 			}
 		}
 	}
 
-	for ($i=0; $i < count($customers); $i++) { 
-		$customers[$i]['billing']['phone'] = str_replace(' ', '', $customers[$i]['billing']['phone']);
-		if ($customers[$i]['billing']['phone'][0] === '0') {
-			$customers[$i]['billing']['phone'] = '+61' . substr($customers[$i]['billing']['phone'], 1);
-		}
-		if ($customers[$i]['billing']['phone'][0] !== '+') {
-			$customers[$i]['billing']['phone'] = '+' . strval($customers[$i]['billing']['phone']);
-		}
-	}
-
-	for ($i=0; $i < count($jsonBooking); $i++) {  //SMS reminder 24 hours before a booking time
-		if (($jsonBooking[$i]['start'] - $long) > 86370 && ($jsonBooking[$i]['start'] - $long) < 86430) {
-			//wp_mail( 'ghjgjh0107@gmail.com', $customers[$i]['billing']['first_name'], $customers[$i]['billing']['phone'] );
-			sendex_publish_post($customers[$i]['billing']['phone'], $customers[$i]['billing']['first_name'], date('H:i', $jsonBooking[$i]['start']));
+	$customers = array();
+	if (is_countable($jsonBooking) && is_countable($jsonOrder)) {
+		for ($i=0; $i < count($jsonBooking); $i++) {
+			$jsonBooking[$i]['start'] = $jsonBooking[$i]['start'] - 37800;
+			$jsonBooking[$i]['end'] = $jsonBooking[$i]['end'] - 37800;
+			for ($j=0; $j < count($jsonOrder); $j++) { 
+				if ($jsonBooking[$i]['order_id'] === $jsonOrder[$j]['id']) {
+					array_push($customers, $jsonOrder[$j]);
+				}
+			}
 		}
 	}
 
-	for ($i=0; $i < count($jsonBooking); $i++) {  //Complete the appointment
-		if (($jsonBooking[$i]['end'] - $long) > -30 && ($jsonBooking[$i]['end'] - $long) < 30) {
-			//wp_mail( 'ghjgjh0107@gmail.com', 'complete appointment', $customers[$i]['billing']['phone'] );
-			complete_appointment($customers[$i]['billing']['phone']);
+	if (is_countable($customers)) {
+		for ($i=0; $i < count($customers); $i++) { 
+			$customers[$i]['billing']['phone'] = str_replace(' ', '', $customers[$i]['billing']['phone']);
+			if ($customers[$i]['billing']['phone'][0] === '0') {
+				$customers[$i]['billing']['phone'] = '+61' . substr($customers[$i]['billing']['phone'], 1);
+			}
+			if ($customers[$i]['billing']['phone'][0] !== '+') {
+				$customers[$i]['billing']['phone'] = '+' . strval($customers[$i]['billing']['phone']);
+			}
+		}
+	}
+
+	if (is_countable($jsonBooking)) {
+		for ($i=0; $i < count($jsonBooking); $i++) {  //SMS reminder 24 hours before a booking time
+			if (($jsonBooking[$i]['start'] - $long) > 86370 && ($jsonBooking[$i]['start'] - $long) < 86430) {
+				//wp_mail( 'ghjgjh0107@gmail.com', $customers[$i]['billing']['first_name'], $customers[$i]['billing']['phone'] );
+				sendex_publish_post($customers[$i]['billing']['phone'], $customers[$i]['billing']['first_name'], date('H:i', $jsonBooking[$i]['start']));
+			}
+		}
+
+		for ($i=0; $i < count($jsonBooking); $i++) {  //Complete the appointment
+			if (($jsonBooking[$i]['end'] - $long) > -30 && ($jsonBooking[$i]['end'] - $long) < 30) {
+				//wp_mail( 'ghjgjh0107@gmail.com', 'complete appointment', $customers[$i]['billing']['phone'] );
+				complete_appointment($customers[$i]['billing']['phone']);
+			}
 		}
 	}
 	//wp_mail( 'ghjgjh0107@gmail.com', $jsonBooking[0]['status'], $customers[0]['billing']['phone'] );
